a171a153755542ab403e3fc49bd3a54440d6edac
oncet <camilorivera86@gmail.com>
2020-05-03 01:12:56 -0300

    WIP

---
+++ src/Body.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box } from '@material-ui/core';
+
+function Body() {
+  return (
+    <Box my={2}>Hello!</Box>
+  );
+}
+
+export default Body;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AppBar, Toolbar } from '@material-ui/core';
+
+function Header() {
+  return (
+    <AppBar>
+      <Toolbar>
+        Hi
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Header;
