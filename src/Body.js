c66354e747271b5efc35d8273d1739d3ddf30f31
oncet <camilorivera86@gmail.com>
2020-05-03 22:03:24 -0300

    WIP

--- src/Body.js
+++ src/Body.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Box } from '@material-ui/core';
+import {
+  Box,
+  Grid
+} from '@material-ui/core';
+import GameCard from './GameCard';
 
 function Body() {
   return (
-    <Box my={2}>Hello!</Box>
+    <Box mt={2}>
+      <Grid container spacing={2}>
+        {[...new Array(12)].map(() => (
+            <Grid item xs={6} sm={4} md={2}>
+              <GameCard />
+            </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
 
---
+++ src/GameCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia
+} from '@material-ui/core';
+
+function GameCard() {
+  return (
+    <Card>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          image="/logo512.png"
+        />
+        <CardContent>
+          Team Fortress 2
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default GameCard;
