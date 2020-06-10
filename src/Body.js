d7426797c1dec08a0c854f4e8c6c2566338d293b
Camilo <camilorivera86@gmail.com>
2020-06-09 21:16:43 -0300

    Get games list from backend

--- src/Body.js
+++ src/Body.js
@@ -1,19 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import {
   Box,
-  Grid
+  CircularProgress,
+  Grid,
+  Button
 } from '@material-ui/core'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import GameCard from './GameCard'
-import steamClient from './steamClient'
+
+const getCookieValueByName = name => document.cookie && document.cookie
+  .split('; ')
+  .find(row => row.startsWith(name))
+  .split('=')[1]
 
 function Body () {
-  const ownedGames = steamClient.getOwnedGames()
+  const [games, setGames] = useState()
+  const userId = getCookieValueByName('userId')
+  useEffect(() => {
+    if (!userId) return
+    fetch(`https://localhost:9000/games/${userId}`)
+      .then(response => response.json())
+      .then(data => setGames(data))
+  }, [userId])
+  if (!userId) return (
+    <Box display="flex" justifyContent="center" mt={10}>
+      <Button
+        color="primary"
+        href="https://localhost:9000/login"
+        startIcon={<AccountCircleIcon />}
+        variant="contained"
+      >
+        Log in
+      </Button>
+    </Box>
+  )
+  if (!games) return (
+    <Box display="flex" justifyContent="center" mt={10}>
+      <CircularProgress />
+    </Box>
+  )
   return (
     <Box mt={2}>
       <Grid container spacing={2}>
-        {ownedGames.map(ownedGame => (
-          <Grid key={ownedGame.appid} item xs={6} sm={4} md={2}>
-            <GameCard game={ownedGame} />
+        {games.slice(0, 60).map(games => (
+          <Grid key={games.appid} item xs={6} sm={4} md={2}>
+            <GameCard game={games} />
           </Grid>
         ))}
       </Grid>
